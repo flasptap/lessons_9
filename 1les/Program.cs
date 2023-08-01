e7af5682ecb0152147e653cf8dc24d9061673cf7
Max <mrbroo123@gmail.com>
2023-08-01 12:15:38 +0300

    test

---
+++ 1les/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintEvenNumbers(int n, int m)
+{
+    n += n % 2;
+    if (n > m)
+    {
+       return;
+    }
+    System.Console.WriteLine(n);
+    PrintEvenNumbers(n + 2, m);
+}
+
+int m = ReadInt("M  ");
+int n = ReadInt("N  ");
+PrintEvenNumbers(m, n);
+
